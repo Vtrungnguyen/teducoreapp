05db4248732d59370c5e559dc5e131dc75623c58
Trung <Trung@TRUNG>
2021-02-06 22:57:24 +0700

    Chua xong

---
+++ TeduCoreApp.Data/Entities/ProductCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TeduCoreApp.Data.Enums;
+using TeduCoreApp.Data.Interfaces;
+
+namespace TeduCoreApp.Data.Entities
+{
+    public class ProductCategory : DomainEntity<int>, IHasSeoMetaData, ISwitchable, ISortable, IDateTracking
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? ParentId { get; set; }
+        public int? HomeOrDer { get; set; }
+        public string Image { get; set; }
+        public bool? HomeFlag { get; set; }
+
+        public string SeoPageTitle { set; get; }
+        public string SeoAlias { set; get; }
+        public string SeoKeyWords { set; get; }
+        public string SeoDescription { set; get; }
+        public Status Status { set; get; }
+        public int SortOrder { set; get; }
+        public DateTime DateCreated { set; get; }
+        public DateTime DateModified { set; get; }
+    }
+}
