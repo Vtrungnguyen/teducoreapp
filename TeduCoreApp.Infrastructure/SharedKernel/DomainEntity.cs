9745c3590a656df8906a7aaf344ed9b715c7a117
Trung <Trung@TRUNG>
2021-02-06 22:19:50 +0700

    Edit Class Domain Entities

---
+++ TeduCoreApp.Infrastructure/SharedKernel/DomainEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace TeduCoreApp.Data.Entities
+{
+    public class DomainEntity<T>
+    {
+        public T ID { get; set; }
+
+        public bool IsTransient()
+        {
+            return ID.Equals(default(T));
+        }
+    }
+}
